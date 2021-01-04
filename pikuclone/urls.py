81be4120c49b845058d1454aa0018ebd7a35c4a0
Exist <bolk9652@naver.com>
2021-01-05 01:16:49 +0900

    🐞fix: add static/media file url

--- pikuclone/urls.py
+++ pikuclone/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
@@ -11,4 +12,6 @@ if settings.DEBUG:
 
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
+        *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+        *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     ]
